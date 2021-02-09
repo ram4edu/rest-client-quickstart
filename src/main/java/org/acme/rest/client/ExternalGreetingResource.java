fd20e42a48c88ef6c7740dde937e92e70a46ea77
Yerramsetty Veera Venkata Rama Krishna <ram.yerramsetty@MKSVISION.LOCAL>
2021-02-09 16:50:45 +0530

    added rest client service

--- src/main/java/org/acme/rest/client/ExternalGreetingResource.java
+++ src/main/java/org/acme/rest/client/ExternalGreetingResource.java
@@ -1,16 +1,25 @@
 package org.acme.rest.client;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.acme.rest.client.quick.ContactExternalService;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 @Path("/getExternalGreeting")
 public class ExternalGreetingResource {
 
+	@Inject
+    @RestClient
+	ContactExternalService contactExternalService;
+	
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    public Response hello() {
+    	return contactExternalService.getGreeting();
+        
     }
 }
